c17d9f6f06c588cf1a4fbaf50b4f56dd7612b937
techamit95ch <amit98ch@gmail.com>
2023-01-09 00:18:02 +0530

    feat: getter and setter

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,13 @@
 import {PersistentUnorderedMap} from "near-sdk-as"
 
-export const courses = new PersistentUnorderedMap<string,string>("COURSES")
+export const courses = new PersistentUnorderedMap<string, string>( "COURSES" )
+
+export function addCourse ( id: string, course: string ): void 
+{
+    courses.set(id,course)
+}
+
+export function getCourse ( id: string ):string|null
+{
+    return courses.get(id)
+}
